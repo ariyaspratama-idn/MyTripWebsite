7d2e2c1e40bb4137950c5ac392255a29670879eb
ariyaspratama-idn <ariyaspratama.id@gmail.com>
2025-10-31 01:16:29 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// your code goes here
+// index.js — interaktif halaman utama
+
+function playMusic() {
+  const music = document.getElementById("bg-music");
+  music.volume = 0.5;
+}
+
+function showAlert() {
+  alert("Selamat menjelajah keindahan wisata Indonesia! 🌴");
+}
